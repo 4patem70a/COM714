1a2f8523c2ccfea93caba9427507f177c2abef8e
Meet Patel <4patem70a@solent.ac.uk>
2020-02-05 14:48:40 +0000

    animal task

---
+++ Week2/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+
+    //attributes
+   private int energy;
+
+   // constructor
+   public Animal() {
+       energy = 10;
+   }
+
+   //behaviours
+   public void eat(int amount ) {
+        energy = energy + amount;
+   }
+   public void speak(){
+       System.out.println("ahh");
+   }
+}
